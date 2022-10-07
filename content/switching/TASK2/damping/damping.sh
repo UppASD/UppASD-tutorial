55616a62339d18f1065c0d3f53b11865f46e0b19
Zhiwei-Lu <51809267+Zhiwei-Lu@users.noreply.github.com>
2022-10-07 22:39:35 +0200

    Update damping.sh

--- content/switching/TASK2/damping/damping.sh
+++ content/switching/TASK2/damping/damping.sh
@@ -7,7 +7,7 @@ do
  cd Damping$alpha
  sed -i "s/DAMPING/$alpha/g" inpsd.dat
  echo -e "Performing simulation for damping $alpha"
- /proj/kthdelin/users/x_zhilu/UppASD_1216_version/UppASD_fp_mipoint/source/sd > out.log 
+ /UppASD/source/sd > out.log 
  echo -e "done"
  cd ..
  
