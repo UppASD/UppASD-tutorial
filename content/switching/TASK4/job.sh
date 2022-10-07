7df96a782747a90d9e5c6a9e22c3f62f134531e5
Zhiwei-Lu <51809267+Zhiwei-Lu@users.noreply.github.com>
2022-10-07 22:40:40 +0200

    Update job.sh

--- content/switching/TASK4/job.sh
+++ content/switching/TASK4/job.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#SBATCH -N 1
-#SBATCH -t 00:20:00
-#SBATCH -A snic2022-1-18
-#SBATCH -J asfe
 
-/proj/kthdelin/users/x_zhilu/UppASD_1216_version/UppASD_fp_mipoint/source/sd 
+/UppASD/source/sd 
 
